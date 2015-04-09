afc09ef55adfabb49b35af06ae06ef6b245a12e8
David Lopez <lopezdp@users.noreply.github.com>
2015-04-08 23:51:08 -0400

    Update HospitalBill.cpp

--- HospitalBill.cpp
+++ HospitalBill.cpp
@@ -1,44 +1,65 @@
 //
-//  HospitalBill.cpp
+//  Hospital.cpp
 //  Ex.03-LopezDP
 //
-//  Created by David P. Lopez on 4/2/15.
+//  Created by David P. Lopez on 4/1/15.
 //  Copyright (c) 2015 David P. Lopez. All rights reserved.
 //
 
-#include "HospitalBill.h"
+#include "Hospital.h"
 
-HospitalBill::HospitalBill()
-{
-    cout << "Inside of default constructor" << endl;
-}
-
-//constructor exhibiting aggregation. a HospitalBill "contains" or "has a" HospitalCharges
-HospitalBill::HospitalBill(string hospitalName, string patientID, double pharmacyCharges, double doctorFees, double roomCharges, double xRayCharges)
-{
+Hospital::Hospital(string hospitalName, string hospitalURL, string hospitalTelephoneNumber, int numDocs, int numPats)
+{   
     this->hospitalName = hospitalName;
-    this->patientID = patientID;
+    this->hospitalURL = hospitalURL;
+    this->hospitalTelephoneNumber = hospitalTelephoneNumber;
     
-    //initialize the bills Object by passing the HospitalBill arguments into the HospitalCharges Constructor
-    bills = HospitalCharges::HospitalCharges(pharmacyCharges, doctorFees, roomCharges, xRayCharges);
-    
-    this->totalcharges = bills.getPharmCharge() + bills.getDocFees() + bills.getRmCharge() + bills.getXRayCharge();
+    Hospital::createDocArr(numDocs);
+    Hospital::createPatArr(numPats, hospitalName);
 }
 
-//print function
-void HospitalBill::print() const
+Hospital::~Hospital()
 {
-    cout << "Hospital Name: " << hospitalName << endl;
-    cout << "Patient ID: " << patientID << endl;
-    
-    cout << "Detailed Hospital Charges:";
-    bills.print();
-    
-    cout << "Total Hospital Charges: " << totalcharges;
+    cout << "Buldozing the Hospital" << endl;
+
+    delete [] ptrDoctors;
+    delete [] ptrPatients;
+    delete [] ptrBill;
 }
 
-HospitalBill::~HospitalBill()
+void Hospital::printDocs()
 {
-    cout << "Closing Hospital Billing System" << endl;
+    cout << endl;
+    cout << "This is the list of Doctors at: " << hospitalName << endl;
+    cout << "==================================================" << endl;
+    cout << endl;
+    
+    for(int i = 0; i < Hospital::numDoctors; i++)
+    {
+        cout << "Doctor #" << i+1 <<"'s Name is: " << ptrDoctors[i].Doctor::getName() << endl;
+        cout << "Doctor #" << i+1 <<"'s Specialty is: " << ptrDoctors[i].Doctor::getSpecialty() << endl;
+        cout << endl;
+    }
 }
 
+void Hospital::printPats()
+{
+    cout << "This is the list of Patients at: " << hospitalName << endl;
+    cout << "==================================================" << endl;
+    cout << endl;
+    
+    for(int i = 0; i < Hospital::numPatients; i++)
+    {
+        cout << "Patient #" << i+1 <<"'s Name is: " << ptrPatients[i].Patient::getName() << endl;
+        cout << "Patient #" << i+1 <<"'s Patient ID is: " << ptrPatients[i].Patient::getPatientID() << endl;
+        cout << "Patient #" << i+1 <<"'s Date of Birth is: " << ptrPatients[i].Patient::getDob() << endl;
+        cout << "Patient #" << i+1 <<"'s Attending Physician is: " << ptrPatients[i].Patient::getAttendingDr() << endl;
+        cout << "Patient #" << i+1 <<"'s Admission Date was: " << ptrPatients[i].Patient::getAdmissionDate() << endl;
+        cout << endl;
+        
+        cout << "This is the Hospital Bill for Patient: " << ptrPatients[i].Patient::getName() << endl;
+        cout << "==================================================" << endl;
+        ptrBill[i].HospitalBill::print();
+        cout << endl;
+    }
+}
