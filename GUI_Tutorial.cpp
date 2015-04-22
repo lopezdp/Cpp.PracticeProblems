89d9bbf397d2dfdc83854a9ef947b8d5b3f0505e
David Lopez <lopezdp@users.noreply.github.com>
2015-04-21 23:00:47 -0400

    Update GUI_Tutorial.cpp

--- GUI_Tutorial.cpp
+++ GUI_Tutorial.cpp
@@ -1,12 +1,30 @@
-#include <QApplication>
-#include <QQmlApplicationEngine>
+#include "guitutorial.h"
+#include "ui_guitutorial.h"
+#include <QtGui>
 
-int main(int argc, char *argv[])
+GoToCellDialog::GoToCellDialog(QWidget *parent) : QDialog(parent)
 {
-    QApplication app(argc, argv);
+    setupUi(this);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-
-    return app.exec();
+    connect(okButton, SIGNAL(clicked()), this, SLOT(accept()));
+    connect(cancelButton, SIGNAL(clicked()), this, SLOT(reject()));
 }
+
+void GoToCellDialog::on_lineEdit_textChanged()
+{
+    okButton->setEnabled(lineEdit->hasAcceptableInput());
+}
+
+/*
+GUITutorial::GUITutorial(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::GUITutorial)
+{
+    ui->setupUi(this);
+}
+
+GUITutorial::~GUITutorial()
+{
+    delete ui;
+}
+*/
